02561cb27f5cbdc168ef4623d7b4c8bc5a88dd8d
Nermin-ab <abdellinermin2022@gmail.com>
2025-04-27 21:11:18 +0100

    test commit

--- app/src/main/java/com/example/chefsjournal/MainActivity3.java
+++ app/src/main/java/com/example/chefsjournal/MainActivity3.java
@@ -1,24 +1,66 @@
 package com.example.chefsjournal;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity3 extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationBarView;
 
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.fragment.app.Fragment;
+
+public class MainActivity3 extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main3);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.fragment_container), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+
+        bottomNavigationView.setSelectedItemId(R.id.menu_home);
+        //afficher menul_home par defaut
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, new HomeFragment())
+                .commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener(){
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                Fragment selected_fragment = null ;
+
+                int id = item.getItemId();
+                if(id == R.id.menu_home){
+                    selected_fragment = new HomeFragment() ;
+                }
+                else if(id == R.id.menu_search){
+                    selected_fragment = new searchFragment();
+                }
+                else if(id == R.id.menu_user){
+                    selected_fragment = new userFragment() ;
+                }
+                if (selected_fragment != null){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selected_fragment).commit();
+                }
+                return true ;
+            }
+        });
+
     }
 }
